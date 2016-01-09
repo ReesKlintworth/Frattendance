25f4031b1879eedefb7c6f7d8faaabc14b502d35
ReesKlintworth <rees.klintworth@gmail.com>
2016-01-08 19:36:44 -0600

    Move methods to server file.

--- server/frattendance.server.js
+++ server/frattendance.server.js
@@ -22,3 +22,59 @@ Meteor.startup(function () {
   });
 }
 });
+
+Meteor.methods({
+  addMember:function(first, last, active){
+    Members.insert({
+      first: first,
+      last: last,
+      active: active
+    });
+  },
+
+  addUser:function(username, email, password){
+    Accounts.createUser({
+       username: username,
+       email: email,
+       password: password,
+       profile: {
+         passwordReset: true
+       }
+    });
+  },
+
+  resetAdminPassword:function(newPassword){
+    var currentUser = Meteor.userId();
+    Accounts.setPassword(currentUser, newPassword, {logout: false});
+    Meteor.users.update({_id: currentUser}, {$set: {"profile.passwordReset": false}});
+  },
+
+  addMeeting:function(date){
+    var meetingId = Meetings.insert({
+      date: moment(date).toDate()
+    });
+
+    var members = Members.find().fetch();
+    for(i = 0; i < members.length; i++) {
+      Meteor.call("addMeetingAttendance", members[i]._id, meetingId);
+    }
+  },
+
+  addMeetingAttendance:function(memberId, meetingId){
+    MeetingAttendance.insert({
+      memberId: memberId,
+      meetingId: meetingId,
+      attended: false
+    });
+  },
+
+  deleteMeeting:function(meetingId){
+    MeetingAttendance.remove({meetingId: meetingId});
+    Meetings.remove(meetingId);
+  },
+
+  toggleAttended:function(meetingId, memberId){
+    var attendance = MeetingAttendance.findOne({$and: [{meetingId: meetingId}, {memberId: memberId}]});
+    MeetingAttendance.update(attendance._id, {$set: {attended: !attendance.attended}});
+  }
+});
